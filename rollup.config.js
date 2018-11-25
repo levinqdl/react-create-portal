7567a1325497ea9e690e8a8572031025c0aed6d1
qudelin <levinqdl@gmail.com>
2018-11-25 14:51:02 +0800

    rebrand to react-create-portal & prepare publishing

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import typescript from "rollup-plugin-typescript"
 module.exports = {
   input: "src/index.tsx",
   output: {
-    dir: "build",
-    file: "bundle.js",
+    dir: "dist",
+    file: "index.js",
     format: "cjs",
   },
   plugins: [typescript()],
-  external: ["react", "react-dom", "react-emotion"],
+  external: ["react", "react-dom"],
 }
