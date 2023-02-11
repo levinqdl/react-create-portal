94443761ce052c496bd86daee6ea259a75c65da1
slivlikh <slivlikh@gmail.com>
2023-02-11 17:35:12 +0200

    issues/17: clean up state in Provider after unmounting Slot (#18)

--- src/PortalProvider.test.tsx
+++ src/PortalProvider.test.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useContext } from "react"
 import PortalProvider from "./PortalProvider"
-import { render } from "@testing-library/react"
+import { render, within } from "@testing-library/react"
+import SlotContext from "./SlotContext"
+import createPortal from "./createPortal"
 
 describe("PortalProvider", () => {
   it("renders children", () => {
@@ -12,5 +14,49 @@ describe("PortalProvider", () => {
     getByText("Hello Portal Layout!")
     const layout = container.firstChild as HTMLElement
     expect(layout).toMatchSnapshot()
+  })
+
+  it("state should be cleaned up after unmounting Slot", () => {
+    const [Slot, Render] = createPortal()
+    const ActiveSlotCount = () => {
+      const { slots } = useContext(SlotContext)
+
+      return <div>Active slots count: {Object.keys(slots).length}</div>
+    }
+
+    const { getByText, rerender } = render(
+      <PortalProvider>
+        <div>
+          Hello Portal <Slot />
+        </div>
+        <Render>Layout!</Render>
+        <ActiveSlotCount />
+      </PortalProvider>
+    )
+    const slotContainer = getByText("Hello Portal")
+    expect(slotContainer).toBeVisible()
+    expect(within(slotContainer).getByText("Layout!")).toBeVisible()
+    expect(getByText("Active slots count: 1")).toBeVisible()
+
+    expect(() =>
+      rerender(
+        <PortalProvider>
+          <div>Hello Portal</div>
+          <Render>Layout!</Render>
+          <ActiveSlotCount />
+        </PortalProvider>
+      )
+    ).not.toThrowError()
+
+    expect(getByText("Active slots count: 0")).toBeVisible()
+
+    expect(() =>
+      rerender(
+        <PortalProvider>
+          <div>Hello Portal</div>
+          <ActiveSlotCount />
+        </PortalProvider>
+      )
+    ).not.toThrowError()
   })
 })
